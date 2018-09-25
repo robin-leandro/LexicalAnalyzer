88cea198adbca4102f6763d70fa98a9b83c5e07a
Ian Duran <ianduran60@gmail.com>
2018-09-25 17:13:36 -0600

    Added QueryLexicalAnalyzer because of git's brainfart Changed main

--- src/main/java/cr/ac/ucr/ci1322/Main.java
+++ src/main/java/cr/ac/ucr/ci1322/Main.java
@@ -6,9 +6,13 @@ import java.io.FileReader;
 public class Main {
     public static void main(String[] args) {
         try{
-            QueryLexicalAnalyzer analyzer = new QueryLexicalAnalyzer(new FileReader(args[0]));
-
-            analyzer.main(args);
+            if(args[1].equals("consulta")) {
+                QueryLexicalAnalyzer analyzer = new QueryLexicalAnalyzer(new FileReader(args[0]));
+                analyzer.main(args);
+            }else{
+                DocumentLexicalAnalyzer analyzer = new DocumentLexicalAnalyzer(new FileReader(args[0]));
+                analyzer.main(args);
+            }
         }catch(FileNotFoundException e){
             e.printStackTrace();
         }
