912203d7372c51241f5300a027826c86604c999e
Fabian Leandro Flores <b53779@ecci.ucr.ac.cr>
2018-09-25 16:58:14 -0600

    Changed the analyzer to specfically analyze queries-.

--- src/main/java/cr/ac/ucr/ci1322/Main.java
+++ src/main/java/cr/ac/ucr/ci1322/Main.java
@@ -6,11 +6,8 @@ import java.io.FileReader;
 public class Main {
     public static void main(String[] args) {
         try{
-            LexicalAnalyzer analyzer = new LexicalAnalyzer(new FileReader(args[0]));
+            QueryLexicalAnalyzer analyzer = new QueryLexicalAnalyzer(new FileReader(args[0]));
 
-            System.out.println(args[1].equals("documento"));
-
-            analyzer.setIsDocument(args[1].equals("documento"));
             analyzer.main(args);
         }catch(FileNotFoundException e){
             e.printStackTrace();
